a81db547c1b21740b4f7a1363e628faf590eef30
razil <razil_khayka@mail.ru>
2023-05-19 22:55:53 +0300

    add services list page

--- RRshop/Controllers/HomeController.cs
+++ RRshop/Controllers/HomeController.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using NGadag.DTO;
+using NGadag.Models;
 using System.Diagnostics;
 
 namespace RRshop.Controllers
@@ -6,17 +9,33 @@ namespace RRshop.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ngadagContext context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ngadagContext context)
         {
             _logger = logger;
+            this.context = context;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            var res = await context.Ads.ToListAsync();
+            foreach (var m in res)
+            {
+                m.Descriptions = m.Descriptions is not null ? Utils.Substring(m.Descriptions, 250) : "null";
+            }
+            return View(res);
         }
-        
+
+        public async Task<IActionResult> Details(Guid id)
+        {
+            var res = await context.Ads
+                .Include(t => t.AdPhotos)
+                .ToListAsync();
+
+            return View(res);
+        }
+
         public IActionResult Privacy()
         {
             return View();
