6e42c9c5964010337ea60951a697315014dece61
razil <razil_khayka@mail.ru>
2023-05-29 00:09:15 +0300

    fix mysql -v

--- NGadag/Program.cs
+++ NGadag/Program.cs
@@ -9,9 +9,6 @@ System.Threading.Thread.CurrentThread.CurrentCulture = new System.Globalization.
 Console.WriteLine("Start 0");
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddDbContext<ngadag>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("ngadag") ?? throw new InvalidOperationException("Connection string 'ngadag' not found.")));
-
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
@@ -21,7 +18,7 @@ builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationSc
 builder.Services.AddAuthorization();
 
 builder.Services.AddDbContext<ngadagContext>(options =>
-options.UseMySql(builder.Configuration.GetConnectionString("MySql"), ServerVersion.Parse("8.0.32-mysql")));
+options.UseMySql(builder.Configuration.GetConnectionString("MySql"), ServerVersion.Parse("8.0.33-mysql")));
 
 builder.Services.AddAutoMapper(typeof(UserMapping));
 
